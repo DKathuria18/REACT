ade1fe390d039ddfce055b195b506089b1f80076
Dhiraj Kathuria <78962786+DKathuria18@users.noreply.github.com>
2023-01-08 13:08:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,103 @@
+// - HMR - Hot module Relaoding
+// - File watcher algorithm
+// - Bundling 
+// - Minifying
+// - Cleaning our code
+// - Dev and Productions build
+// - Super fast Build algortihms
+// - Image optimiztaions
+// - Coaching while Developemnt
+// - Compression
+// - Compatible with older version
+// - HTTPs on development
+// - Port number
+// - Consistent hashing algoritms
+// - Zero conifguration
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
+// const heading1 = React.createElement(
+//     "h1", 
+//     {
+//      id: "title",
+//      key: "h2"
+
+//     },
+//      "Heading 1 from parcel");
+
+
+// react.createElement gives us an object
+
+// const heading2 = React.createElement(
+//     "h2", 
+//     {
+//      id: "title",
+//      key: "h1"
+//     },
+//      "Heading 2");
+
+
+//      const container = React.createElement(
+//       "div",
+//       {
+//         id: "container",
+//       },
+//       [heading1, heading2]
+//      )
+
+
+// There is a another way of writing using JSX
+// jsx is converting react.createElement behind the jsx code as written below
+// we can also add class a className using camelcase 
+
+    //  const heading = (
+    //   <h1 id="heading" key="h1">
+    //     NAMASTE
+    //   </h1>
+    //  )
+
+
+
+     ////////////////////////////////////////////////////////////////////////
+    // fUNCTIONAL COMPONENTS
+
+//this is react element becuase of arrow function () =>
+// const Header = (
+//    <h1>Namaste react</h1>
+// )
+
+
+//this is react component of aroow function ()=>
+// const Header2 = () => {return(
+//   <div>
+//     {Header}
+//     <h1>Namaste functional Component</h1>
+//     <h2>Namaste functional Component</h2>
+//   </div>
+// );
+// };
+
+
+
+
+
+
+     // const root = ReactDOM.createRoot(document.getElementById("root"))
+  // const root = ReactDOM.createRoot(document.getElementById("root"))
+
+  // passing the react element in the root
+  // root.render(Header2)
+
+  // root.render(<Header />)
+
+  // root.render(<Header2 />)
+
+
+  // console.log(heading)
+
+
+  // lec 4 Talk is cheap show me the code
+  // Our own app food
+                                    //  FOOD
